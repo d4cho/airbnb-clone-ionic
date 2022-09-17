1444c13a0a595fe8f1c5b87d32ea90895a2f3d34
bioDanCho <daniel.cho@thebioagency.com>
2022-09-17 13:37:53 -0400

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
+    IonApp,
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet,
+    IonTabBar,
+    IonTabButton,
+    IonTabs,
+    setupIonicReact,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { heart, chatbox, search, person } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -34,43 +34,61 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Icons */
+import { FaAirbnb } from 'react-icons/fa';
+
 setupIonicReact();
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+    <IonApp>
+        <IonReactRouter>
+            <IonTabs>
+                <IonRouterOutlet>
+                    <Route exact path='/Explore'>
+                        <Tab1 />
+                    </Route>
+                    <Route exact path='/Wishlists'>
+                        <Tab2 />
+                    </Route>
+                    <Route path='/Trips'>
+                        <Tab3 />
+                    </Route>
+                    <Route path='/Inbox'>
+                        <Tab3 />
+                    </Route>
+                    <Route path='/Profile'>
+                        <Tab3 />
+                    </Route>
+                    <Route exact path='/'>
+                        <Redirect to='/Explore' />
+                    </Route>
+                </IonRouterOutlet>
+
+                <IonTabBar slot='bottom'>
+                    <IonTabButton tab='Explore' href='/Explore'>
+                        <IonIcon icon={search} />
+                        <IonLabel>Explore</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab='Wishlists' href='/Wishlists'>
+                        <IonIcon icon={heart} />
+                        <IonLabel>Wishlists</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab='Trips' href='/Trips'>
+                        <FaAirbnb style={{ fontSize: 26, paddingBottom: 2 }} />
+                        <IonLabel>Trips</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab='Inbox' href='/Inbox'>
+                        <IonIcon icon={chatbox} />
+                        <IonLabel>Inbox</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab='Profile' href='/Profile'>
+                        <IonIcon icon={person} />
+                        <IonLabel>Profile</IonLabel>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonTabs>
+        </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
