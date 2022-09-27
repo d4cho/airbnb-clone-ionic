c87527961f476292ab63021dca1f344aad647937
bioDanCho <daniel.cho@thebioagency.com>
2022-09-27 09:17:19 -0400

    clear input icon

--- src/components/atoms/TextInput/TextInput.jsx
+++ src/components/atoms/TextInput/TextInput.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TextInput.scss';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 const TextInput = (props) => {
     const { icon } = props;
+    const [textValue, setTextValue] = useState('');
 
     return (
         <div className='TextInput_container'>
             {icon && <div className='input_icon'>{icon}</div>}
             <input
                 type='text'
+                value={textValue}
+                onChange={(e) => setTextValue(e.target.value)}
                 placeholder='Search reviews'
                 style={{ paddingLeft: icon ? '3rem' : '1rem' }}
             />
+            {textValue && (
+                <div className='clear_icon'>
+                    <AiFillCloseCircle
+                        size={26}
+                        onClick={() => setTextValue('')}
+                    />
+                </div>
+            )}
         </div>
     );
 };
